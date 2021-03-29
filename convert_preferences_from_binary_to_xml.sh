9e381049a1ae36413538246db2bf917a1fce1dee
issenn <issennknight@gmail.com>
2021-03-29 08:45:48 +0800

    Add: add some cmd and configuration.

--- convert_preferences_from_binary_to_xml.sh
+++ convert_preferences_from_binary_to_xml.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env zsh
 
 
-plutil -convert xml1 Library/Preferences/com.googlecode.iterm2.plist
+# plutil -convert xml1 Library/Preferences/com.googlecode.iterm2.plist
+
+echo "File: $@"
+plutil -convert xml1 "$@"
